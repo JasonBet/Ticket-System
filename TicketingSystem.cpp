5659b680d1f8a15ce26af757d8d0e25d90194b90
Jason B <jasonbetsargon@gmail.com>
2023-12-15 23:23:51 -0800

    completed definitions in cpp file for class

--- TicketingSystem.cpp
+++ TicketingSystem.cpp
@@ -8,3 +8,18 @@ void TicketingSystem::processTicket(){
     tickets.push(newTicket);
     std::cout<<"Ticket generated: " << newTicket.id<<std::endl;
 }
+
+void TicketingSystem::processTicket(){
+    if(!tickets.empty()){
+        Ticket currentTicket=tickets.front();
+        tickets.pop();
+        std::cout<<"Ticket processed: "<<currentTicket.id<<std::endl;
+    }
+    else{
+        std::cout<<"No tickets to process."<<std::endl;
+    }
+}
+
+int TicketingSystem::queueSize() const{
+    return tickets.size();
+}
